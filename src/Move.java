1d3b0ac1bd2e2440eba8d57b4632225f42a08b2f
fkodanrc <fkodanrc@hotmail.com>
2013-11-28 18:26:37 -0200

    Update Move.java

--- src/Move.java
+++ src/Move.java
@@ -1,30 +1,30 @@
-/*
-	Os métodos desta classe são todos estáticos por que ela serve 
-	somente para retornar direções, não é necessário criar um objeto
-	para instanciá-la.
-	
-	Uma abstração que pode ser feita é que o tabuleiro pode mover-se somente
-	de um jeito durante o tempo.
+/* 
+ * Esta classe e responsavel por manter informacoes sobre movimentacoes realizadas
+ *
+ * Esta classe nao pode ser static, pois todo TileBoard precisa 
+ * ter uma representacao da sequencia de movimentos pra chegar nele, e,
+ * por isso, precisa instanciar um objeto desta classe a cada movimento.
  */
+
 public class Move{
 	public static final int LEFT = 0;
 	public static final int UP = 1;
 	public static final int RIGHT = 2;
 	public static final int DOWN = 3;
 
-	private static int direction;
-	private static int oppositeDirection;
+	private int direction;
+	private int oppositeDirection;
 
 	public Move(int dir){
 		direction = dir;
 		oppositeDirection = (direction + 2)%4;
 	}
 	
-	public static int getDirection(){
+	public int getDirection(){
 		return direction;
 	}
 	
-	public static int getOppositeDirection(){
+	public int getOppositeDirection(){
 		return oppositeDirection;
 	}
 }
