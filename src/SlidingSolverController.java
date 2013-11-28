c4c281edd9630101cca83d8c7b7138d121319aea
fkodanrc <fkodanrc@hotmail.com>
2013-11-28 18:29:44 -0200

    Update and rename SlidingSolverDriver.java to SlidingSolverController.java

---
+++ src/SlidingSolverController.java
@@ -0,0 +1,9 @@
+/* Esta classe e responsavel por fazer a chamada para a criacao do agente.
+ * Ela devera ser removida com a utilizacao de JADE.
+ */
+
+public class SlidingSolverController {
+	public static void main(String[] args){
+		SlidingSolver ss = new SlidingSolver();
+	}
+}
